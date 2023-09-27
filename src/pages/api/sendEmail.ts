3d0e83c081e0772c5013920139110caba5b23312
ETCasual <casualorient@gmail.com>
2023-09-27 17:22:57 +0800

    allow POST only

--- src/pages/api/sendEmail.ts
+++ src/pages/api/sendEmail.ts
@@ -8,10 +8,7 @@ const enableCORS =
   async (req: NextApiRequest, res: NextApiResponse) => {
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Origin", "*"); // Replace this with your actual origin
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, DELETE, PATCH, POST, PUT",
-    );
+    res.setHeader("Access-Control-Allow-Methods", "POST");
     res.setHeader(
       "Access-Control-Allow-Headers",
       "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
