0db5d285b20ce88eb912e2712ce732e37362f8a2
ETCasual <casualorient@gmail.com>
2023-09-27 12:03:58 +0800

    modify CORS config

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,11 +11,10 @@ export function middleware() {
   const res = NextResponse.next();
 
   // add the CORS headers to the response
-  res.headers.append("Access-Control-Allow-Credentials", "true");
   res.headers.append("Access-Control-Allow-Origin", "*");
   res.headers.append(
     "Access-Control-Allow-Methods",
-    "GET,DELETE,PATCH,POST,PUT",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS",
   );
   res.headers.append(
     "Access-Control-Allow-Headers",
